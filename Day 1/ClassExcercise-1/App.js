f63685b6812860176a93ebd181aea75da7ab145e
Yukesh PS <108916377+Yukesh420@users.noreply.github.com>
2023-04-05 15:31:46 +0530

    Add files via upload

--- Day 1/ClassExcercise-1/App.js
+++ Day 1/ClassExcercise-1/App.js
@@ -1,1 +1,6 @@
-hi
+console.log("Message to Developer")
+function AlertMsg(){
+    const Hi=()=>{alert("Message from Javascript Alert!")}
+    return <div><button onClick={Hi}>Click</button></div>
+}
+ReactDOM.render(<AlertMsg/>,document.getElementById('my div'))
